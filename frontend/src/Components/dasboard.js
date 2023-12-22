ad7c09690a64cdce7ba5491cc1f3bccff268c1fa
DaimAhmad <143156641+DaimAhmad@users.noreply.github.com>
2023-12-22 12:07:47 +0500

    main

--- frontend/src/Components/dasboard.js
+++ frontend/src/Components/dasboard.js
@@ -85,7 +85,7 @@ const Dashboard = () => {
             />
             <div className="profile-name">DAIM AHMAD</div>
             <div className="profile-email">{localStorage.getItem('adminEmail')}</div>
-            <button onClick={handleLogoutAdmin}  style={{marginRight:"200px"}}>Log out</button>
+            <button onClick={handleLogoutAdmin}  style={{marginRight:"159px"}}>Log out</button>
           </div>
         </nav>
       </aside>
